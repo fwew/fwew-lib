33a4fdd6f741c2a6f721546b0a612edd0b8164a9
Tirea Aean <tirea@learnnavi.org>
2022-02-28 19:55:47 -0600

    Update version

--- version.go
+++ version.go
@@ -27,7 +27,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	5, 4, 0,
+	5, 4, 1,
 	"dev",
 	"Maweya Mawup",
 	"15.4.3",
