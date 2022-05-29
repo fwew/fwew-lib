fc34cee80d7f60878bd82990069099749e2cc97c
Tirea Aean <tirea@learnnavi.org>
2022-05-29 03:36:54 -0500

    Add additional punctuation chars to the strip list

--- version.go
+++ version.go
@@ -27,7 +27,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	5, 6, 1,
+	5, 6, 2,
 	"dev",
 	"Maweya Mawup",
 	"15.4.3",
