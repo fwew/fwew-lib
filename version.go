2897736068dce4e498c3f7e4b2c2ed15cd6caa29
Corey Scheideman <corscheid@gmail.com>
2023-05-02 19:24:33 -0500

    5.8.2: patch -usia genitives

--- version.go
+++ version.go
@@ -27,7 +27,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	5, 8, 1,
+	5, 8, 2,
 	"dev",
 	"Maweya Mawup",
 	"15.4.3",
