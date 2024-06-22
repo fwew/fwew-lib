c917fa7e16707e47c717838c8a109d9c72f27489
Corey Scheideman <corscheid@gmail.com>
2024-06-21 23:41:46 -0500

    Update version

--- version.go
+++ version.go
@@ -26,7 +26,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	5, 14, 0,
+	5, 14, 1,
 	"",
 	"Maweya Mawup",
 	"",
