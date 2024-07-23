9721ef7c7c938a9f8764e20e4466957a965881e2
Corey Scheideman <corscheid@gmail.com>
2024-07-23 18:31:42 -0500

    Enable language ko

--- txt.go
+++ txt.go
@@ -257,7 +257,7 @@ func init() {
 	texts["tip"] = "type \"/help\" or \"/commands\" for more info"
 	texts["author"] = "Tirea Aean"
 	texts["header"] = fmt.Sprintf("%s\n%s\n", Version, texts["tip"])
-	texts["languages"] = "de, en, es, et, fr, hu, nl, pl, pt, ru, sv, tr, uk"
+	texts["languages"] = "de, en, es, et, fr, hu, ko, nl, pl, pt, ru, sv, tr, uk"
 	texts["POSFilters"] = "allvtr.n.num.pn.adv.adj.vin.v.inter.part.svin.adp.adv., n.vtrm.vim.conj.pn., sbd.n., intj.intj."
 	texts["POSFilters"] += "vtrm., vtr.part., intj.vin., svin.prop.n.affixvin., intj.dem.dem., n.sbd.n., adv."
 	texts["POSFilters"] += "adj., n.adj., adv.adj., intj.dem., pn.vtr., vin.adv., intj.pn., adv.ph.vin., vtr.adj.,  conj."
