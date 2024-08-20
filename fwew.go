105dd3691e58b0748309a1395d300f59b13edd7f
lizardgai4 <lizardgai4@gmail.com>
2024-08-19 17:23:54 -0700

    Removed unneeded thing

--- fwew.go
+++ fwew.go
@@ -1181,19 +1181,5 @@ func StartEverything() {
 	PhonemeDistros()
 	elapsed := strconv.FormatFloat(time.Since(start).Seconds(), 'f', -1, 64)
 
-	var g = func(word Word) error {
-		//reefy := strings.ReplaceAll(ReefMe(word.IPA, false)[0], "-", "")
-		foresty := strings.ReplaceAll(word.IPA, "ʊ", "u")
-		if foresty == word.IPA {
-			fmt.Println("NULL")
-		} else {
-			fmt.Println(foresty)
-		}
-		//fmt.Println()
-		return nil
-	}
-
-	runOnFile(g)
-
 	fmt.Println("Everything is cached.  Took " + elapsed + " seconds")
 }
