d2c00801acb9654f1efda27ec4f95d21a0b86e68
lizardgai4 <lizardgai4@gmail.com>
2024-11-09 14:22:15 -0800

    Saved a bunch of string/rune conversions

--- fwew.go
+++ fwew.go
@@ -1040,11 +1040,11 @@ func dialectCrunch(query []string, guaranteedForest bool) []string {
 
 		nucleusCount := 0
 		// remove reef tìftangs
-		for _, b := range []string{"a", "ä", "e", "i", "ì", "o", "u", "ù", "ll", "rr"} {
+		for i, b := range []string{"a", "ä", "e", "i", "ì", "o", "u", "ù", "ll", "rr"} {
 			if strings.Contains(a, b) {
 				nucleusCount += strings.Count(a, b)
-				for _, c := range []string{"a", "ä", "e", "i", "ì", "o", "u", "ù", "ll", "rr"} {
-					if b != "ll" && c != "ll" && b != "rr" && c != "rr" {
+				for j, c := range []string{"a", "ä", "e", "i", "ì", "o", "u", "ù", "ll", "rr"} {
+					if i < 8 && j < 8 {
 						a = strings.ReplaceAll(a, b+"'"+c, b+c)
 					}
 				}
