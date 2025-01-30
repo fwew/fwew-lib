90aa88033107136159cc88a40fe21708434d5e15
Corey Scheideman <corscheid@gmail.com>
2025-01-30 02:23:18 -0600

    Merge PR#74, update version

--- version.go
+++ version.go
@@ -26,7 +26,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	5, 22, 3,
+	5, 22, 4,
 	"",
 	"Kanua Kenten",
 	"",
