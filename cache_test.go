cfb62f9787aa03532aff6ab89b7e06db142fc231
lizardgai4 <lizardgai4@gmail.com>
2025-09-11 12:40:53 -0700

    Made it better at advising awkawardness in forest dialect

--- cache_test.go
+++ cache_test.go
@@ -23,7 +23,7 @@ func Test_cacheDict(t *testing.T) {
 		ET:             "katsuma, puutuma",
 		FR:             "toucher",
 		HU:             "(meg)érint",
-		IT:             "uno",
+		IT:             "12",
 		KO:             "만지다",
 		NL:             "aanraken",
 		PL:             "dotykać",
