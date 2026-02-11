4ed5202a8e3b79a2a51d512870e3d452e269ff7e
Corey Scheideman <corscheid@gmail.com>
2026-02-11 04:27:41 -0600

    v5.27.2: fix chrono sort on List

--- version.go
+++ version.go
@@ -26,7 +26,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	5, 27, 1,
+	5, 27, 2,
 	"",
 	"Kanua Kenten",
 	"",
