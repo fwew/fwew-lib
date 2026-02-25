eb5bcb9f41ca2193cd539f76efe52b0da95bfa95
Corey Scheideman <corscheid@gmail.com>
2026-02-24 22:18:35 -0600

    Refactor NameAlu, extract functions to simplify logic

--- name_gen.go
+++ name_gen.go
@@ -7,38 +7,6 @@ import (
 	"strings"
 )
 
-type phonemeTuple struct {
-	value  int
-	letter string
-}
-
-type tuples []phonemeTuple
-
-func (t tuples) Len() int {
-	return len(t)
-}
-
-func (t tuples) Swap(i, j int) {
-	t[i], t[j] = t[j], t[i]
-}
-
-func (t tuples) Less(i, j int) bool {
-	// bigger values first here
-	if t[i].value == t[j].value {
-		return AlphabetizeHelper(t[i].letter, t[j].letter)
-	}
-	return t[i].value > t[j].value
-}
-
-func formatAdj(adj string, twoWordNoun bool, dialect int) string {
-	if !twoWordNoun && (adj[0] != 'a' || dialect != 1) {
-		return "a" + glottalCaps(adj)
-	} else if twoWordNoun && (adj[len(adj)-1] != 'a' || dialect != 1) {
-		return glottalCaps(adj) + "a"
-	}
-	return glottalCaps(adj)
-}
-
 // SingleNames generates single given names
 func SingleNames(nameCount int, dialect int, syllableCount int) (output string) {
 	universalLock.Lock()
@@ -159,194 +127,29 @@ func NameAlu(nameCount int, dialect int, syllableCount int, nounMode int, adjMod
 		output += glottalCaps(singleNameGen(randIfZero(syllableCount), dialect))
 
 		/* Noun */
-		nmode := 0
-		if nounMode != 1 && nounMode != 2 {
-			nmode = rand.Intn(5) // 80% chance of normal noun
-			if nmode == 4 {
-				nmode = 2
-			} else {
-				nmode = 1
-			}
-		} else {
-			nmode = nounMode
-		}
-
 		twoWordNoun := false
+		noun := getNameAluNoun(getNMode(nounMode), dialect, allNouns, allVerbs)
 
-		noun := ""
-		switch nmode {
-		case 1:
-			nounWord := fastRandom(allNouns)
-			noun += strings.ReplaceAll(convertDialect(nounWord, dialect), "-", "")
-		default: // case 2:
-			verb := fastRandom(allVerbs)
-			a := strings.Split(convertDialect(verb, dialect), " ")
-			for k := range a {
-				noun += a[k]
-			}
-			noun = strings.ReplaceAll(noun, "-", "")
-			noun += "yu"
-		}
-
+		/* Alu */
 		output += " alu"
 
+		/* Noun */
 		if len(strings.Split(noun, " ")) > 1 {
 			twoWordNoun = true
 		} else {
 			output += " " + glottalCaps(noun)
 		}
 
+		/* Adjective */
 		if adjMode != 1 {
-			// Adjective
-			amode := 0
-			switch adjMode {
-			case 0:
-				// "something" mode
-				amode = rand.Intn(8) - 1
-				if amode <= 2 {
-					// 50% chance of normal adjective
-					amode = 2
-				} else if amode >= 5 {
-					// Verb participles get two sides of the die
-					amode = 5
-				}
-			case -1:
-				// "any" mode
-				amode = rand.Intn(5) + 1
-			default:
-				amode = adjMode
-			}
-
-			adj := ""
-			switch amode {
-			// no case 1 (no adjective)
-			case 2: // normal adjective
-				adjWord := fastRandom(allAdjectives)
-				adj = convertDialect(adjWord, dialect)
-				adj = strings.ReplaceAll(adj, "-", "")
-
-				// If the adj starts with "a" in forest, we don't need another a
-				if !twoWordNoun && (strings.ToLower(string(adj[0])) != "a" || dialect != 1) {
-					if (adj[:2] == "le" && adj != "ler" && adj != "leyr" && adj != "lewnga'") || adj == "lafyon" {
-						adj = glottalCaps(adj) // le-adjectives
-					} else {
-						adj = "a" + glottalCaps(adj)
-					}
-				} else if twoWordNoun && (adj[len(adj)-1] != 'a' || dialect != 1) {
-					adj = glottalCaps(adj) + "a"
-				} else {
-					adj = glottalCaps(adj) // forest dialect a-adjectives like axpa or alaksi
-				}
-			case 3: //genitive noun
-				adjWord := fastRandom(allNouns)
-
-				adj = strings.ToLower(adjWord.Navi)
-				switch adj {
-				case "tsko swizaw":
-					adj = "Tsko Swizawyä"
-				case "toruk makto", "torùk makto":
-					if dialect == 0 || dialect == 2 {
-						adj = "Torùkä Maktoyuä"
-					} else {
-						adj = "Torukä Maktoyuä"
-					}
-				case "mo a fngä'":
-					adj = "Moä a Fgnä'"
-				default:
-					adj = convertDialect(adjWord, dialect)
-					adjSplit := strings.Split(adj, " ")
-					if hasAt("aeìiä", adjSplit[0], -1) {
-						adjSplit[0] = adjSplit[0] + "yä"
-					} else {
-						adjSplit[0] = adjSplit[0] + "ä"
-					}
-					adj = ""
-					for _, a := range adjSplit {
-						adj += glottalCaps(a) + " "
-					}
-					adj = strings.TrimSuffix(adj, " ")
-				}
-
-				adj = strings.ReplaceAll(adj, "-", "")
-			case 4: //origin noun
-				adjWord := fastRandom(allNouns)
-				adj = strings.ToLower(adjWord.Navi)
-				switch adj {
-				case "tsko swizaw":
-					adj = "ta Tsko Swizaw"
-				case "toruk makto", "torùk makto":
-					if dialect == 0 || dialect == 2 {
-						adj = "ta Torùkä Maktoyu"
-					} else {
-						adj = "ta Torukä Maktoyu"
-					}
-				case "mo a fngä'":
-					adj = "ta Mo a Fgnä'"
-				default:
-					adj = convertDialect(adjWord, dialect)
-					if twoWordNoun {
-						adj = glottalCaps(adj) + "ta"
-					} else {
-						adj = "ta " + glottalCaps(adj)
-					}
-				}
-
-				adj = strings.ReplaceAll(adj, "-", "")
-			case 5: //participle verb
-				infix := "us"
-				findVerb := oneWordVerb(allVerbs)
-				// If it's transitive, 50% chance of <awn>
-				if findVerb.PartOfSpeech[2] == 'r' && rand.Intn(2) == 0 {
-					infix = "awn"
-				}
-				adj = findVerb.InfixDots
-				switch dialect {
-				case 2: // reef
-					adj = quickReef(adj)
-					fallthrough
-				case 0: // interdialect
-					adj = specialU(adj, findVerb.IPA)
-				}
-
-				adj = insertInfix(strings.Split(adj, " "), infix)
-				// If the adj starts with "a" in forest, we don't need another a
-				adj = formatAdj(adj, twoWordNoun, dialect)
-			case 6: //active participle verb
-				findVerb := oneWordVerb(allVerbs)
-				adj = findVerb.InfixDots
-				switch dialect {
-				case 2: // reef
-					adj = quickReef(adj)
-					fallthrough
-				case 0: // interdialect
-					adj = specialU(adj, findVerb.IPA)
-				}
-
-				adj = insertInfix(strings.Split(adj, " "), "us")
-
-				// If the adj starts with "a" in forest, we don't need another a
-				adj = formatAdj(adj, twoWordNoun, dialect)
-			case 7: //passive participle verb
-				findVerb := oneWordVerb(allTransitiveVerbs)
-				adj = findVerb.InfixDots
-				switch dialect {
-				case 2: // reef
-					adj = quickReef(adj)
-					fallthrough
-				case 0: // interdialect
-					adj = specialU(adj, findVerb.IPA)
-				}
-
-				adj = insertInfix(strings.Split(adj, " "), "awn")
-				// If the adj starts with "a" in forest, we don't need another a
-				adj = formatAdj(adj, twoWordNoun, dialect)
-			}
+			adj := getNameAluAdjective(getAdjMode(adjMode), allAdjectives, dialect, twoWordNoun, allNouns, allVerbs, allTransitiveVerbs)
 
 			if len(adj) > 1 {
 				output += " " + adj
 			}
 		}
 
+		/* Two word noun */
 		if twoWordNoun {
 			output += " "
 			nounWords := strings.SplitSeq(noun, " ")
