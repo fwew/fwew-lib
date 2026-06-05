5f990bae2cdf9ba52aa424cd00536e62ac7aff8a
lizardgai4 <lizardgai4@gmail.com>
2026-06-04 19:38:47 -0700

    Converts syllable breakdown to lowercase to find more

--- list.go
+++ list.go
@@ -602,7 +602,7 @@ func filterWord(results []Word, word Word, args []string, checkDigraphs uint8) [
 	case 2: // 2: compress syllables, but not spec (find fake digraphs)
 		syllables = compress(strings.ToLower(syllables))
 		navi = compress(strings.ToLower(navi))
-	default:
+	default: // The dictionary breaks down Eywa as Ey-wa with the uppercase
 		syllables = strings.ToLower(syllables)
 	}
 
