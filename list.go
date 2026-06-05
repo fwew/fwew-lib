78b77a72b34fe5e34be570410cfd41edff3d5f16
lizardgai4 <lizardgai4@gmail.com>
2026-06-04 19:36:23 -0700

    Converts syllable breakdown to lowercase to find more

--- list.go
+++ list.go
@@ -602,6 +602,8 @@ func filterWord(results []Word, word Word, args []string, checkDigraphs uint8) [
 	case 2: // 2: compress syllables, but not spec (find fake digraphs)
 		syllables = compress(strings.ToLower(syllables))
 		navi = compress(strings.ToLower(navi))
+	default:
+		syllables = strings.ToLower(syllables)
 	}
 
 	syllables = strings.ReplaceAll(syllables, "-", "")
